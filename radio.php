c27e8e092eff13b83d04f948dd2294407aa90c56
Lucas Dapont <129424234+LucasDapont@users.noreply.github.com>
2023-11-07 14:24:58 -0300

    Add files via upload

---
+++ radio.php
@@ -0,0 +1,11 @@
+<?php
+    $sistema=$_POST["sistema"];
+    $monitor=$_POST["monitor"];
+    $placa=$_POST["placa"];
+    $mouse=$_POST["mouse"];
+    echo $sistema."<br>".$monitor."<br>".$placa."<br>".$mouse;
+
+?>
+
+
+
