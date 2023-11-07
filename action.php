aa48de72bdcc52cd9c488f7ab198aca393c2a966
Lucas Dapont <129424234+LucasDapont@users.noreply.github.com>
2023-11-07 14:41:59 -0300

    Add files via upload

---
+++ action.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta lang="pt-br">
+    </head>
+    <body>
+        <form action="action2.php" method="post">
+            <fieldset>
+                <legend>Formulário</legend>
+                <p>
+                    <label for="nome">Nome:</label>
+                    <input type="text" name="nome" id="nome">
+                </p>
+                <p>
+                    <label for="email">E-mail:</label>
+                    <input type="email" name="email" id="email">
+                </p>
+                <br>
+                <p>
+                    <input type="submit" value="Enviar" id="enviar">
+                </p>
+            </fieldset>
+        </form>
+
+    </body>
---
+++ action2.php
@@ -0,0 +1,4 @@
+<?php
+    echo "Nome: " .($_POST["nome"] ?? ""). "<br>";echo "E-mail: " .($_POST["email"] ?? "")."<br>";
+
+?>
